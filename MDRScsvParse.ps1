7055fa9b33990b2f787eb2317ebb56472ba916d1
Jordan Landry <jlandry0312@gmail.com>
2019-07-11 01:12:09 -0500

    Semi-working csv parse

--- MDRScsvParse.ps1
+++ MDRScsvParse.ps1
@@ -1,18 +1,20 @@
-﻿$pathToCsv = "C:\Users\jlandry\Documents\Research\xxxx-Master Document Register_data.csv"
-$content = Get-Content -Path $pathToCsv
+﻿$pathToCsv = "C:\Users\Jland\Google Drive\Stella Maris\PS_Excel-Project\PS_Excel-Project\xxxx-Master Document Register_data.csv"
+#$content = Get-Content -Path $pathToCsv
 
-$content -notmatch'(^[\s,-]*$)|(rows\s*affected)'|Set-Content -Path $pathToCsv
-$content | Select-Object -Skip 7 | Set-Content -Path $pathToCsv
+#$content -notmatch'(^[\s,-]*$)|(rows\s*affected)'|Set-Content -Path $pathToCsv
+#$content | Select-Object -Skip 7 | Set-Content -Path $pathToCsv
 
 $smDWG = @()
 $camDWG = @()
 $pSubDate = @()
 
-$P = Import-Csv -Path $pathToCsv | ForEach-Object {
-    $smDWG += $_."STELLA MARIS"
-    $camDWG += $_."CAMERON"
-    $pSubDate += $_."PLANNED INI
+Import-Csv -Path $pathToCsv
+ForEach-Object {
+    $smDWG += $_."STELLA MARIS DOC. / DRAWING NO."
+    #$camDWG += $_."CAMERON"
+    #$pSubDate += $_."PLANNED INITIAL SUBMITTAL DATE"
+}
 
-
+#Write-Host $smDWG
 
 
---
+++ columnparse.ps1
@@ -0,0 +1,3 @@
+Get-Process -Name WmiPrvSE | Select-Object -Property BasePriority,Id,SessionId,WorkingSet |
+  Export-Csv -Path .\WmiData.csv -NoTypeInformation
+Import-Csv -Path .\WmiData.csv
